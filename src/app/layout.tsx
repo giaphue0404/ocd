81ec7f9c6eb30bf35e0c7484a9314504cea5db1a
giaphue0404 <giaphue0404@gmail.com>
2025-11-29 17:15:58 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import '@/assets/css/index.css';
 import DisableDevtool from '@/components/disable-devtool';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import type { Metadata } from 'next';
 import { Roboto, Roboto_Mono } from 'next/font/google';
+import { headers } from 'next/headers'
 config.autoAddCss = false;
 const robotoSans = Roboto({
     variable: '--font-roboto-sans',
@@ -14,7 +14,18 @@ const robotoMono = Roboto_Mono({
     variable: '--font-roboto-mono',
     subsets: ['latin']
 });
-export const dynamic = 'force-dynamic';
+
+export const generateMetadata = async () => {
+  const h = await headers()
+  const host = h.get('x-forwarded-host') || h.get('host')
+  const proto = h.get('x-forwarded-proto') || 'https'
+  const base = `${proto}://${host}`
+
+  return {
+    metadataBase: new URL(base)
+  }
+}
+
 const RootLayout = ({
     children
 }: Readonly<{
@@ -23,7 +34,7 @@ const RootLayout = ({
     return (
         <html lang='en' data-scroll-behavior='smooth'>
             <body className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}>
-                {/* <DisableDevtool /> */}
+                <DisableDevtool />
                 {children}
             </body>
         </html>
