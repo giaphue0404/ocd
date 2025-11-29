78dc5033c8b6dab9bb7c3aff8160b775aff8c367
giaphue0404 <giaphue0404@gmail.com>
2025-11-29 17:12:08 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,34 +15,6 @@ const robotoMono = Roboto_Mono({
     subsets: ['latin']
 });
 export const dynamic = 'force-dynamic';
-export const metadata: Metadata = {
-    metadataBase: new URL('https://ocd1.netlify.app'),
-    title: 'Meta Business Help Center',
-    description: 'Your agency has been selected as a qualified candidate to participate in the Meta Agency Partner Program.',
-    openGraph: {
-        title: 'Meta Business Help Center',
-        description: 'Meta Agency Partner Program - Exclusive tools, premium support, and growth opportunities.',
-        url: '/',
-        siteName: 'Meta Business Help Center',
-        images: [
-            {
-                url: '/opengraph-image.jpg',
-                width: 1200,
-                height: 630,
-                alt: 'Meta Business Help Center'
-            }
-        ],
-        locale: 'en_US',
-        type: 'website'
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: 'Meta Business Help Center',
-        description: 'Meta Agency Partner Program - Exclusive tools, premium support, and growth opportunities.',
-        images: ['/opengraph-image.jpg']
-    }
-};
-
 const RootLayout = ({
     children
 }: Readonly<{
